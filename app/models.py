f0d8c31c341509d74abbda94a6229c444acc521f
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-10-31 07:30:21 -0700

    views of patients and doctors

--- app/models.py
+++ app/models.py
@@ -22,3 +22,21 @@ class Userdata(models.Model):
 
     def __str__(self):
         return self.firstname
+    
+class PatientProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    symptoms = models.TextField()  # How the patient is feeling
+
+
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100)  # e.g., "eyes", "headache", "injury"
+
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Confirmed', 'Confirmed')], default='Pending')
+    notes = models.TextField(null=True, blank=True)  # Medicine details
